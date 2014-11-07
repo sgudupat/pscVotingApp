e507e0bf5dd7e9e6f8282094f6aef7bf3796de20
sgudupat <sandeep.g@pscglobalsolutions.com>
2014-11-07 23:20:50 +0530

    Added another test method

--- firstApp/src/main/java/firstApp/App.java
+++ firstApp/src/main/java/firstApp/App.java
@@ -4,4 +4,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public void testHelloWorld() {
+        System.out.println("Adding another method!");
+    }
 }
