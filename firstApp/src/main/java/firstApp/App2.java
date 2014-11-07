74d9b25db135d6b0c2337099fedda19d4fac3756
sgudupat <sandeep.g@pscglobalsolutions.com>
2014-11-07 23:31:37 +0530

    Added a new file

---
+++ firstApp/src/main/java/firstApp/App2.java
@@ -0,0 +1,11 @@
+package firstApp;
+
+public class App2 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public void testHelloWorld() {
+        System.out.println("Adding another method!");
+    }
+}
